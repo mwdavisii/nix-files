dace94a0fb908bafa37b93131eab1fdf749f4a55
Mike D <mwdavisii@gmail.com>
2023-01-06 21:53:34 -0600

    01062023

--- home.nix
+++ home.nix
@@ -2,13 +2,14 @@
 
 
 
-let 
-  mkPersistentLink = path: pkgs.runCommand "persistent-link" {} ''
-    builtins.trace ''ln -s $HOME/${path}''
-    ln -s $HOME/${path} $out
-  '';
+#let 
+#  mkPersistentLink = path: pkgs.runCommand "persistent-link" {} ''
+#    builtins.trace ''ln -s $HOME/${path}''
+#    ln -s $HOME/${path} $out
+#  '';
 
-in {
+#in 
+{
   # Home Manager needs a bit of information about you and the
   # paths it should manage.
   home.username = "mwdavisii";
@@ -22,10 +23,19 @@ in {
   # You can update Home Manager without changing this value. See
   # the Home Manager release notes for a list of state version
   # changes in each release.
-  home.stateVersion = "22.11";
+  home.stateVersion = "23.05";
 
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
+
+  programs.vim = {
+    enable = true;
+    plugins = with pkgs.vimPlugins; [ vim-airline ];
+    settings = { ignorecase = true; };
+    extraConfig = ''
+      set mouse=a
+    '';
+  };
 
   #packages to install
   home.packages = [
@@ -38,12 +48,11 @@ in {
   home.file.".p10k.zsh".source  = ./.p10k.zsh;
   home.file.".zprofile".source = ./.zprofile;
   home.file.".profile".source = ./.profile;
+  home.file.".config/nvim/init.vim".source = ./.config/nvim/init.vim;
 
   programs.neovim = {
     enable = true;
     viAlias = true;
     vimAlias = true;
   };
-    
-  home.file.".config".source = mkPersistentLink ".config";
 }
