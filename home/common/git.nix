ffe7314fb2892f4b520b7b8001f890ceb5be1290
Mike D <mwdavisii@gmail.com>
2023-02-07 14:41:59 -0600

    updated git

--- home/common/git.nix
+++ home/common/git.nix
@@ -4,5 +4,11 @@
 {
     programs.git = {
         enable = true;
+        userName = "Mike D.";
+        userEmail = "mwdavisii@gmail.com";
+        extraConfig = {
+            url."ssh://git@github.com".insteadOf = "https://github.com/";
+            init.defaultBranch = "main";
+        };
     };
 }
--- home/home.nix
+++ home/home.nix
@@ -12,14 +12,9 @@
       
     ];
   };
-  #home.file.".zshrc".source = ../dotfiles/.zshrc;
-  home.file.".bashrc".source = ../dotfiles/.bashrc;
-  home.file.".gitconfig".source = ../dotfiles/.gitconfig;
   home.file.".nix-channels".source = ../dotfiles/.nix-channels;
-  home.file.".profile".source = ../dotfiles/.profile;
   home.file.".config/nvim/init.vim".source = ../dotfiles/.config/nvim/init.vim;
-  #home.file.".config/starship/starship.toml".source = ../dotfiles/.config/starship/starship.toml;
-  
+
   imports = [
     ./apps/default.nix
     ./shell/default.nix
