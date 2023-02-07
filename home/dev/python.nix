8146f9488c8b74d039320e63113930b415a5985f
Mike D <mwdavisii@gmail.com>
2023-02-07 11:09:30 -0600

    python

--- home/dev/python.nix
+++ home/dev/python.nix
@@ -1,41 +1,18 @@
-{ config, lib, pkgs, ... }:
+{ lib, pkgs, ... }:
 
-with lib;
-let
-  cfg = config.mwdavisii.modules.dev.python;
-  configHome = config.xdg.configHome;
-  dataHome = config.xdg.dataHome;
-  in
-{
-  options.nyx.modules.dev.python = {
-    enable = mkEnableOption "python configuration";
-
-    extraPackages = mkOption {
-      description = "List of extra packages to be installed";
-      type = with types; listOf package;
-      default = with pkgs.python39Packages; [
-        black
-        jedi
-        pip
-        poetry
-        pylint
-        setuptools
-      ];
-    };
-  };
-
-  config = mkIf cfg.enable {
-    home.packages = with pkgs; [
-      python39
-      pipenv
-    ] ++ cfg.extraPackages;
-
-    # home.sessionVariables = {
-    #   PIP_CONFIG_FILE = "${configHome}/pip/pip.conf";
-    #   PIP_LOG_FILE = "${dataHome}/pip/log";
-    #   PYLINTHOME = "${dataHome}/pylint";
-    #   PYLINTRC = "${configHome}/pylint/pylintrc";
-    #   PYTHONSTARTUP = "${configHome}/python/pythonrc";
-    # };
-  };
-}
+pkgs.python3.withPackages (p: with p; [
+  aiohttp # async HTTP
+  beautifulsoup4 # web scraping
+  folium # maps
+  geopy # geographical data
+  ipython # interactive shell
+  networkx # graphs
+  numpy # numerical computation
+  pandas # data analysis
+  pylint # static checking
+  pwntools
+  requests # HTTP library
+  setuptools # setup.py
+  z3 # Z3 theorem prover
+  venv
+])
