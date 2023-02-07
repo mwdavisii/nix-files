02ffbed2a5e94e06cce938122f5228ec4a4f38b5
Mike D <mwdavisii@gmail.com>
2023-02-07 11:46:11 -0600

    k9s

--- home/shell/default.nix
+++ home/shell/default.nix
@@ -3,6 +3,7 @@
 {
   imports = [ 
     ./exa.nix
+    ./k9s.nix
     ./nvim.nix 
     ./starship.nix 
     ./vim.nix 
---
+++ home/shell/k9s.nix
@@ -0,0 +1,7 @@
+{ config, pkgs, ... }:
+
+{
+programs.k9s = {
+    enable = true;
+  };
+}
