69d16c3c9c703230c034b28704dbea131a54fdc1
Mike D <66962344+mwdavisii@users.noreply.github.com>
2023-02-07 23:02:33 -0600

    Update git.nix

--- home/common/git.nix
+++ home/common/git.nix
@@ -7,7 +7,7 @@
         userName = "Mike D.";
         userEmail = "mwdavisii@gmail.com";
         extraConfig = {
-            url."ssh://git@github.com/".insteadOf = "https://github.com/";
+            url."ssh://git@github.com/uLabSystems/".insteadOf = "https://github.com/uLabSystems/";
             init.defaultBranch = "main";
         };
     };
