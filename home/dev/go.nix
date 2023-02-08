a926e46b6189b9ba29aaa86b97e5383f25ca4548
Mike D <mwdavisii@gmail.com>
2023-02-07 20:28:39 -0600

    fixed go

--- home/dev/go.nix
+++ home/dev/go.nix
@@ -7,8 +7,8 @@
             "golang.org/x/text" = builtins.fetchGit "https://go.googlesource.com/text";
             "golang.org/x/time" = builtins.fetchGit "https://go.googlesource.com/time";
         };
-        goPath = "/home/mwdavis/go";
-        goBin = "~/.nix-profile/bin/";
+        goPath = "/home/mwdavisii/go";
+        goBin = "home/mwdavisii/.nix-profile/bin/";
         goPrivate = [
             "github.com/uLabSystems"
         ];
